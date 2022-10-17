6ac65260395e15e4b9aaabf81c474d100923dad4
Christopher Slaughter <cslaugh1@umbc.edu>
2022-10-17 19:28:17 -0400

    login-test-01

--- database/login.py
+++ database/login.py
@@ -2,6 +2,6 @@ import sqlite3 as sql
 
 con = sql.connect("job-seeker.db")
 cur = con.cursor()
-statement = "SELECT username, password FROM users"
+statement = "SELECT username, password FROM user"
 cur.execute(statement)
 print(cur.fetchall())
