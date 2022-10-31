3b20025b7c7c1d5d8668092b2d95d87196002e8a
Christopher Slaughter <cslaughter306@gmail.com>
2022-10-31 06:29:29 -0400

    Login Page Base and Welcome Screen Start

--- job-seeker/src/App.js
+++ job-seeker/src/App.js
@@ -1,25 +1,52 @@
+import React, { useState } from 'react';
 import logo from './logo.svg';
-import './App.css';
-import Dashboard from './components/Dashboard/Dashboard';
-import Preferences from './components/Preferences/Preferences';
+import './index.css';
+import LoginForm from './components/LoginForm';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 function App() {
+  const adminUser = {
+    email: "admin@jobseeker.com",
+    password: "password"
+  }
+
+  const [user, setUser] = useState({name: "", email: ""});
+  const [error, setError] = useState("");
+
+  const Login = details => {
+    console.log(details);
+
+    if (details.email == adminUser.email && details.password == adminUser.password){
+      console.log("Logged In");
+      setUser({
+        name: details.name,
+        email: details.email
+      })
+    } else {
+      console.log("Details do not match")
+      setError("Details do not match!")
+    }
+
+  }
+
+  const Logout = () => {
+    console.log("Logout")
+    setUser({
+      name: "", 
+      email: ""
+    })
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {(user.email != "") ? (
+        <div className="welcome">
+        <h2>Welcome to Job Seeker, <span>{user.name}</span></h2>
+        <button onClick={Logout}>Logout</button>
+        </div>
+      ) : (
+        <LoginForm Login={Login} error={error} />
+      )}
     </div>
   );
 }
--- job-seeker/src/Routes.js
+++
@@ -1,7 +0,0 @@
-import Login from "../containers/Login";
-
-<Route exact path="/login">
-
-  <Login />
-
-</Route>
