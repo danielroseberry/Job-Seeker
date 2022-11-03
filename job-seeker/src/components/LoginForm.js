c496f881016e30d97651a62127d4b08987837247
Christopher Slaughter <cslaughter306@gmail.com>
2022-11-03 11:51:18 -0400

    Color changes began UserPortal

--- job-seeker/src/components/LoginForm.js
+++ job-seeker/src/components/LoginForm.js
@@ -12,21 +12,23 @@ function LoginForm({ Login, error}) {
   return (
     <form onSubmit={submitHandler}>
         <div className="form-inner">
-        <h2>Login</h2>
-        {(error != "") ? (<div className="error">{error}</div>) : ""}
-        <div className="form-group">
-            <label htmlFor="name">Name:</label>
-            <input type="text" name="name" id="name" onChange={e => setDetails({...details, name: e.target.value})} value={details.name} />
-        </div>
-        <div className="form-group">
-            <label htmlFor="email">Email:</label>
-            <input type="text" name="email" id="email" onChange={e => setDetails({...details, email: e.target.value})} value={details.email}/>
-        </div>
-        <div className="form-group">
-            <label htmlFor="password">Password:</label>
-            <input type="password" name="password" id="password" onChange={e => setDetails({...details, password: e.target.value})} value={details.password}/>
-        </div>
-        <input type="submit" value="LOGIN"/>
+            <h2>JobSeeker</h2>
+            {(error != "") ? (<div className="error">{error}</div>) : ""}
+            <div className="form-group">
+                <label htmlFor="name">Name:</label>
+                <input type="text" name="name" id="name" onChange={e => setDetails({...details, name: e.target.value})} value={details.name} />
+            </div>
+            <div className="form-group">
+                <label htmlFor="email">Email:</label>
+                <input type="text" name="email" id="email" onChange={e => setDetails({...details, email: e.target.value})} value={details.email}/>
+            </div>
+            <div className="form-group">
+                <label htmlFor="password">Password:</label>
+                <input type="password" name="password" id="password" onChange={e => setDetails({...details, password: e.target.value})} value={details.password}/>
+            </div>
+            <div className="login-button-container">
+                <input type="submit" value="LOGIN"/>
+            </div>
         </div>
     </form>
   )
---
+++ job-seeker/src/components/UserPortal.js
@@ -0,0 +1,12 @@
+// import React, { useState } from 'react'
+// import './'
+
+// function UserPortal() {
+
+//     return(
+//         <div className='UserPortal'>
+//             <h2>Welcome to Job Seeker, <span>{user.name}</span></h2>
+//             <button onClick={Logout}>Logout</button>
+//         </div>
+//     );
+// }
